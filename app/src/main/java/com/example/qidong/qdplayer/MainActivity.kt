3066e5735d5364fa5a8be3f13e3de19ea867d416
qidong <qidong@bakheet.cn>
2019-04-02 11:02:22 +0800

    添加注释

--- app/src/main/java/com/example/qidong/qdplayer/MainActivity.kt
+++ app/src/main/java/com/example/qidong/qdplayer/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.qidong.qdplayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * @author qidong
+ * @date 2019-04-02
+ * desc 主页面
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
